e83ad8ddc2130bd68ac2be688f7d1d2933d1a3bf
Janet Lee <lee.cjanet@gmail.com>
2017-10-27 13:20:43 -0700

    timer displays on webpage

--- Widgets/frontend/clock.jsx
+++ Widgets/frontend/clock.jsx
@@ -8,6 +8,16 @@ class Clock extends React.Component {
       time: new Date(),
     };
 
+    this.tick = this.tick.bind(this);
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnMount() {
+    clearInterval(this.intervalId);
+    this.intervalId = 0;
   }
 
   tick() {
@@ -17,9 +27,20 @@ class Clock extends React.Component {
   }
 
   render() {
+    let hour = this.state.time.getHours();
+    let minute = this.state.time.getMinutes();
+    let second = this.state.time.getSeconds();
+    let midday = (hour < 12) ? 'AM' : 'PM';
+
+    hour = (hour > 12) ? (hour - 12) : hour;
+    hour = (hour < 10) ? `0${hour}` : hour;
+    minute = (minute < 10) ? `0${minute}` : minute;
+    second = (second < 10) ? `0${second}` : second;
+
     return(
       <div>
         <h1>Beginning of something beautiful</h1>
+        <p>{hour} : {minute} : {second} {midday}</p>
       </div>
     );
   }
