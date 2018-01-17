b445c5686210c8a2295722887a20399d276e9a36
Janet Lee <lee.cjanet@gmail.com>
2018-01-16 17:56:09 -0800

    consolelogstime

---
+++ w5d5/functions.js
@@ -0,0 +1,62 @@
+class Clock {
+  constructor() {
+    // 1. Create a Date object.
+    // 2. Store the hours, minutes, and seconds.
+    // 3. Call printTime.
+    // 4. Schedule the tick at 1 second intervals. 
+    const date = new Date()
+    this.ss = date.getSeconds();
+    this.mm = date.getMinutes();
+    this.hr = date.getHours();
+    
+    this.printTime = this.printTime.bind(this);
+    this._tick = this._tick.bind(this);
+    this.addSeconds = this.addSeconds.bind(this);
+    this.addMinute = this.addMinute.bind(this);
+    this.addHour = this.addHour.bind(this);
+  }
+
+  printTime() {
+    // Format the time in HH:MM:SS
+    // Use console.log to print it.
+    
+    console.log(`${this.hr}:${this.mm}:${this.ss}`)
+  }
+
+  _tick() {
+    // 1. Increment the time by one second.
+    // 2. Call printTime.
+    setInterval(this.addSeconds, 1000);
+    setInterval(this.printTime, 1000);
+  }
+  
+  addSeconds() {
+    this.ss += 1
+    
+    if (this.ss > 59) {
+      this.ss = 0;
+      this.addMinute();
+    }
+  }
+  
+  addMinute() {
+    this.mm += 1
+    
+    if (this.mm > 59) {
+      this.mm = 0;
+      this.addHour();
+    }
+  }
+  
+  addHour() {
+    this.hr += 1
+    
+    if (this.hr > 12) {
+      this.hr = 1;
+    }
+  }
+}
+
+const clock = new Clock();
+
+clock._tick()
