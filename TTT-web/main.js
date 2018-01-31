65526bea6d62574e569e2028970d0a228b4dcba6
Janet Lee <lee.cjanet@gmail.com>
2018-01-31 15:45:08 -0800

    remove eventlistener if game is over

--- TTT-web/main.js
+++ TTT-web/main.js
@@ -21,31 +21,31 @@ function startGame() {
 	origBoard = Array.from(Array(9).keys());
 	console.log(origBoard);
 	for (let i = 0; i < cells.length; i++) {
-		console.log(cells[i])
 		cells[i].addEventListener('click', turnClick)
 	}
 }
 
 function turnClick(e) {
 	return turn(e.target.id, huPlayer);
-	// console.log(e.target.classList.addClass);
-	// e.className += " " + "active";
-	// console.log(e.className);
 }
 
 function turn(squareId, player) {
 	cells[squareId].innerText = player;
 	origBoard[squareId] = player;
-	const checkWin = winner(origBoard, player)
+	const gameWon = checkWin(origBoard, player);
+	if (gameWon) {
+		gameOver(gameWon);
+	}
 }
 
 function checkWin(board, player) {
   // find the idx where player has played. concat idx if el == player
-	let plays = board.reduce((accum, el, i) => (el === player)) ? a.concat(i) : accum, []);
+	let plays = board.reduce((accum, el, i) => (el === player) ? accum.concat(i) : accum, []);
+	console.log(plays)
 	let gameWon = null;
   // [index, win] == find the index of the winCombos AND return the w
 	for (let [index, win] of winCombos.entries()) {
-		if (win.every(elem => plays.indexOf(elem > -1))) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
       // index: winning combo, player: which player won
 			gameWon = {index: index, player: player};
 			break;
@@ -54,28 +54,12 @@ function checkWin(board, player) {
 	return gameWon;
 }
 
-function gameOver() {
-	// console.log(cells[1].addClass('active'))
-  if (winner()) {
-	  return true;
-  }
-	
-  return cells.every(cell => {
-    return (cell.innerText !== "");
-  });
-}
-
-function winner() {
-  for (let idx=0; idx < winCombos.length; idx++) {
-    if (winCombos[idx].every(i => {
-			return cells[i].innerText === huPlayer
-		})) {
-      return huPlayer;
-    } else if (winCombos[idx].every(i => {
-			return cells[i].innerText === aiPlayer;
-		})) {
-      return aiPlayer;
-    }
-  }
-  return null;
+function gameOver(gameWon) {
+	for (let index of winCombos[gameWon.index]) {
+		document.getElementById(index).style.backgroundColor = 
+			gameWon.player == huPlayer ? "blue" : "red";
+	}
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false)
+	}
 }
